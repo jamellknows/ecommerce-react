bd8a0812f73270d20d2bf7c6060c03ab47599d3c
jamellknows <jamellsamuels@googlemail.com>
2021-03-17 21:42:42 +0000

    share code on github

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:3.9,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:95,
+            brand:'Lacoste',
+            rating:4.8,
+            numReviews:10,
+            description:'high quality product'
+
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Joggers',
+            category:'Joggers',
+            image:'/images/p4.jpg',
+            price:78,
+            brand:'Nike',
+            rating:4.9,
+            numReviews:9,
+            description:'high quality product'
+
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Joggers',
+            category:'Joggers',
+            image:'/images/p5.jpg',
+            price:65,
+            brand:'Puma',
+            rating:3.5,
+            numReviews:12,
+            description:'high quality product'
+
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Joggers',
+            category:'Joggers',
+            image:'/images/p6.jpg',
+            price:90,
+            brand:'Adida',
+            rating:4.2,
+            numReviews:15,
+            description:'high quality product'
+
+        },
+    ],
+}
+
+export default data;
